36db325bf3706fd3f3bc79359cd1fc0a68071a85
Roy Smith <roy@panix.com>
2022-01-20 22:04:41 +0000

    Add search demo.

---
+++ search/search.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from configparser import ConfigParser
+import json
+from pathlib import Path
+from pprint import pprint
+import sys
+
+from opensearchpy import OpenSearch
+
+
+CONFIGS = (Path.home() / '.elasticsearch.ini',
+           Path('elasticsearch.ini'))
+
+
+def main():
+    config = ConfigParser()
+    config.read(CONFIGS)
+    user = config.get('elasticsearch', 'user')
+    password = config.get('elasticsearch', 'password')
+    server = config.get('elasticsearch', 'server')
+    index_name = config.get('elasticsearch', 'index')
+
+    es = OpenSearch(server, http_auth=(user, password))
+
+    #pprint(es.indices.get(index_name))
+    query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"user": "AusTerrapin"}},
+                    ]
+                }
+            }
+        }
+
+    pprint(es.search(body=query, index=index_name))
+
+if __name__ == '__main__':
+    main()
