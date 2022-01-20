c1ff649c3a5e99dbfc953143a56be06786621287
Roy Smith <roy@panix.com>
2022-01-20 22:03:54 +0000

    Add document creation, config file.

--- search/elastic.py
+++ search/elastic.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 
+from configparser import ConfigParser
+import json
+from pathlib import Path
 from pprint import pprint
+import sys
+
 from opensearchpy import OpenSearch
 
 
+CONFIGS = (Path.home() / '.elasticsearch.ini',
+           Path('elasticsearch.ini'))
+
+
 def main():
+    config = ConfigParser()
+    config.read(CONFIGS)
+    user = config.get('elasticsearch', 'user')
+    password = config.get('elasticsearch', 'password')
+    server = config.get('elasticsearch', 'server')
+    index_name = config.get('elasticsearch', 'index')
 
-    es = OpenSearch(['elasticsearch.svc.tools.eqiad1.wikimedia.cloud:80'])
-    pprint(es.indices.get('*'))
+    es = OpenSearch(server, http_auth=(user, password))
 
+    es.indices.create(index_name, ignore=400)
+
+    for line in sys.stdin:
+        revision = json.loads(line)
+        es.index(index_name, revision)
 
 if __name__ == '__main__':
     main()
