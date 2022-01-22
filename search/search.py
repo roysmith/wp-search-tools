f2bc72a7a7a5b5bc369b81146192a5d787d4bf00
Roy Smith <roy@panix.com>
2022-01-22 02:54:17 +0000

    Add --stats and --search to search.py

--- search/search.py
+++ search/search.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser
 from configparser import ConfigParser
 import json
+import logging
 import os
 from pathlib import Path
 from pprint import pprint
@@ -10,8 +12,19 @@ import sys
 from opensearchpy import OpenSearch
 
 
-
 def main():
+    logging.basicConfig(level=logging.WARNING)
+    parser = ArgumentParser()
+    parser.add_argument('--config', '-c', default='config.ini',
+                        help='config file (default %(default)s)')
+    parser.add_argument('--file', '-f',
+                        help='input file (over-rides config')
+    parser.add_argument('--stats', action='store_true',
+                        help='print index stats')
+    parser.add_argument('--search', nargs='*',
+                        help='search term')
+    args = parser.parse_args()
+
     config = ConfigParser()
     config.read(get_configs())
     user = config.get('elasticsearch', 'user')
@@ -21,17 +34,29 @@ def main():
 
     es = OpenSearch(server, http_auth=(user, password))
 
+    if args.search:
+        return search(es, index_name, args.search)
+    if args.stats:
+        return stats(es, index_name)
+
+
+def search(es, index_name, words):
     query = {
         "query": {
             "bool": {
                 "must": [
-                    {"match": {"user": "AusTerrapin"}},
+                    {"match": {"comment": ' '.join(words)}},
                     ]
                 }
             }
         }
 
     pprint(es.search(body=query, index=index_name))
+
+
+def stats(es, index_name):
+    data = es.count(index=index_name)
+    print(f'index "{index_name}" has {data.get("count")} items')
 
 
 def get_configs():
